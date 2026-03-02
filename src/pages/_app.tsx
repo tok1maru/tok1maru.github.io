3ba7a8bfbf389c9e8dba75a5af441a0244bf95ec
BuildTools <unconfigured@null.spigotmc.org>
2026-03-02 16:41:43 +0900

    addpages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ConfigProvider } from 'antd';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: 'Bahnschrift, Meirio', // Ant Design の全体フォント
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
